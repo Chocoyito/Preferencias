95e91f3a7848bb83326bbc21548221dad2d2d8b2
Chocoyito <josarroliga0904@gmail.com>
2022-06-08 18:49:38 -0600

    Adios basura, hola dinero xd

---
+++ Modulos/General/AutoVender.lua
@@ -0,0 +1,14 @@
+local x = CreateFrame("Frame")
+x:RegisterEvent("MERCHANT_SHOW")
+local function eventoTienda()
+    local enlace
+    for mochila = 0, 4 do
+        for casilla = 0, GetContainerNumSlots(mochila) do
+            enlace = GetContainerItemLink(mochila, casilla)
+            if enlace and (select(3, GetItemInfo(enlace)) == 0) then
+                UseContainerItem(mochila, casilla)
+            end
+        end
+    end
+end
+x:SetScript("OnEvent", eventoTienda)
