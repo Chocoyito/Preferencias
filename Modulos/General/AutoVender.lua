5c1ad40d8bfa80a030ed42eb56d87c24fc16db9e
Chocoyito <josarroliga0904@gmail.com>
2022-06-08 18:58:27 -0600

    Un 1 xd

--- Modulos/General/AutoVender.lua
+++ Modulos/General/AutoVender.lua
@@ -3,7 +3,7 @@ x:RegisterEvent("MERCHANT_SHOW")
 local function eventoTienda()
     local enlace
     for mochila = 0, 4 do
-        for casilla = 0, GetContainerNumSlots(mochila) do
+        for casilla = 1, GetContainerNumSlots(mochila) do
             enlace = GetContainerItemLink(mochila, casilla)
             if enlace and (select(3, GetItemInfo(enlace)) == 0) then
                 UseContainerItem(mochila, casilla)
